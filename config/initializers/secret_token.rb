a60e70fd212960fdc3a1f653163054c46c393e79
Albert Agram <albert.agram@gmail.com>
2013-10-23 09:56:47 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FraudStarr::Application.config.secret_key_base = 'c593ba4d6e0db1f7920afd5d26f24c9f1f5995737eef5e70bed793cfb6bc16c45784e49c021cf1bca296bd540645108b5343aa67314b17b588bb43ada0596684'
