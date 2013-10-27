b28e8b70725b56cb5ac569a33b45df8b651b5ded
Albert Agram <albert.agram@gmail.com>
2013-10-27 08:13:46 +0000

    initial setup for dynamic forms

---
+++ app/controllers/fraud_types_controller.rb
@@ -0,0 +1,2 @@
+class FraudTypesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 FraudStarr::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+
+  get 'register_fraud', to: 'frauds#register'
+
+  namespace :admin do
+    resources :fraud_types
+  end
+
+  resources :frauds, only: [:new, :create]
 end
