5351a985e2e73e6cdfd17f314f52856dc6e2a1eb
Albert Agram <albert.agram@gmail.com>
2013-11-07 18:22:15 +0000

    search route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ FraudStarr::Application.routes.draw do
     resources :fraud_types
   end
 
-  resources :frauds, only: [:new, :create]
+  resources :frauds, only: [:new, :create] do
+    collection do
+      get 'search', to: 'frauds#search'
+    end
+  end
 end
