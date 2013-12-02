bdc076365767687f3e32b2ba0840365ae3c10dbb
Albert Agram <albert.agram@gmail.com>
2013-12-02 12:25:10 +0000

    User signs in spec

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature "user signs in" do
+  scenario "with valid email and password" do
+    bob = Fabricate(:user)
+    visit sign_in_path
+    fill_in "email", with: bob.email
+    fill_in "password", with: bob.password
+    click_button "Sign In"
+    expect(page).to have_content "Report Fraud!"
+  end
+end
