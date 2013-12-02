7927d1c19a2f9891ed1d055205efbd2e8b26f4ca
Albert Agram <albert.agram@gmail.com>
2013-12-02 13:36:02 +0000

    Sessions controller spec create action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,5 +14,37 @@ describe SessionsController do
     end
   end
 
+  describe "POST create" do
+    context "with valid credentials" do
+      it "puts the signed in user in the session" do
+        david = Fabricate(:user)
+        post :create, email: david.email, password: david.password
+        expect(session[:user_id]).to eq(david.id)
+      end
 
+      it "redirects to root path" do
+        david = Fabricate(:user)
+        post :create, email: david.email, password: david.password
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "with invalid credentials" do
+      it "does not put the siged in user in the session" do
+        david = Fabricate(:user)
+        post :create, email: david.email, password: david.password + 'asdfght'
+        expect(session[:user_id]).to be_nil
+      end
+      it "redirects to the sign in page" do
+        david = Fabricate(:user)
+        post :create, email: david.email, password: david.password + 'asdfght'
+        expect(response).to redirect_to sign_in_path
+      end
+      it "sets the flash error message" do
+        david = Fabricate(:user)
+        post :create, email: david.email, password: david.password + 'asdfght'
+        expect(flash[:error]).not_to be_blank
+      end
+    end
+  end
 end
