f0f057bbf60e2d11c44ee39319b7c22521188e96
Albert Agram <albert.agram@gmail.com>
2013-12-10 15:18:24 +0000

    Removed unnecessary code from User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
   has_many :accounts, dependent: :destroy
 
-  def has_twitter?
-    accounts.where(provider: 'twitter').any?
-  end
-
-  def has_facebook?
-    accounts.where(provider: 'facebook').any?
-  end
 end
