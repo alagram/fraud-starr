a2ed8ea1c02782ae3729122c5bacf4f1812bc552
Albert Agram <albert.agram@gmail.com>
2013-12-27 07:25:54 +0000

    modified admin fraud_types controller spec
    require user before filter added

--- spec/controllers/admin/fraud_types_controller_spec.rb
+++ spec/controllers/admin/fraud_types_controller_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe Admin::FraudTypesController do
   describe "GET new" do
+    it_behaves_like "requires sign in" do
+      let(:action) { get :new }
+    end
     it "sets @fraud_type to a new FraudType" do
+      set_current_user
       get :new
       expect(assigns(:fraud_type)).to be_instance_of FraudType
       expect(assigns(:fraud_type)).to be_new_record
