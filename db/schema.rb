4f477d8d7ec4955d7f8a25e57fde27145e208533
Albert Agram <albert.agram@gmail.com>
2013-12-30 12:19:44 +0000

    migration to add admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131217154254) do
+ActiveRecord::Schema.define(version: 20131230082136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20131217154254) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
+    t.boolean  "admin"
   end
 
 end
