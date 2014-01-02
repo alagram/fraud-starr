620708d8a8803f31b91d35e89cdb53d722f94ab1
Albert Agram <albert.agram@gmail.com>
2014-01-02 07:56:17 +0000

    admin frauds controller spec

--- spec/controllers/admin/frauds_controller_spec.rb
+++ spec/controllers/admin/frauds_controller_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe Admin::FraudsController do
-  describe "GET show" do
+  describe "GET edit" do
+
+    it_behaves_like "requires sign in" do
+      fraud = Fabricate(:fraud)
+      let(:action) { get :edit, id: fraud.id }
+    end
+
+    it "sets @fraud" do
+      set_current_admin
+      fraud = Fabricate(:fraud)
+      get :edit, id: fraud.id
+      expect(assigns(:fraud)).to eq(fraud)
+    end
+
+    it "redirects the regular user to the root path" do
+      fraud = Fabricate(:fraud)
+      set_current_user
+      get :edit, id: fraud.id
+      expect(response).to redirect_to root_path
+    end
+
+    it "sets the flash error message for regular user" do
+      fraud = Fabricate(:fraud)
+      set_current_user
+      get :edit, id: fraud.id
+      expect(flash[:error]).to be_present
+    end
   end
 end
