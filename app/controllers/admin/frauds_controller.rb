06c4a2f83e27328727b28f3cc01b747d28b52ae5
Albert Agram <albert.agram@gmail.com>
2014-01-03 11:03:18 +0000

    admin frauds controller spec

--- app/controllers/admin/frauds_controller.rb
+++ app/controllers/admin/frauds_controller.rb
@@ -1,2 +1,27 @@
 class Admin::FraudsController < ApplicationController
+  before_action :require_user
+  before_action :require_admin
+  before_action :set_fraud
+
+  def edit
+  end
+
+  def update
+    if @fraud.update(fraud_params)
+      flash[:success] = "Fraudulent event successfully updated."
+      redirect_to edit_admin_fraud_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_fraud
+    @fraud = Fraud.includes(:images).find(params[:id])
+  end
+
+  def fraud_params
+    params.require(:fraud).permit!
+  end
 end
--- spec/controllers/admin/frauds_controller_spec.rb
+++ spec/controllers/admin/frauds_controller_spec.rb
@@ -29,4 +29,59 @@ describe Admin::FraudsController do
       expect(flash[:error]).to be_present
     end
   end
+
+  describe "PATCH update" do
+
+    context "with valid attibutes" do
+
+      it "redirects the regular user to the root path" do
+        fraud = Fabricate(:fraud)
+        set_current_user
+        patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud)
+        expect(response).to redirect_to root_path
+      end
+
+      it "sets @fraud" do
+        set_current_admin
+        fraud = Fabricate(:fraud)
+        patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud)
+        expect(assigns(:fraud)).to eq(fraud)
+      end
+
+      it "changes attributes of fraudulent event" do
+        set_current_admin
+        fraud = Fabricate(:fraud)
+        patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud, status: "3")
+        expect(fraud.reload.status).to eq("3")
+      end
+
+      it "should redirect_to edit admin fraud path" do
+        set_current_admin
+        fraud = Fabricate(:fraud)
+        patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud, status: "3")
+        expect(response).to redirect_to edit_admin_fraud_path
+      end
+    end
+
+    context "with invalid attibutes" do
+      it "does not change fraud attibutes" do
+        set_current_admin
+        fraud = Fabricate(:fraud)
+        patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud, title: nil, status: "3")
+        expect(fraud.reload.status).to_not eq("3")
+      end
+
+      it "renders edit page" do
+        set_current_admin
+        fraud = Fabricate(:fraud)
+        patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud, title: nil, status: "3")
+        expect(response).to render_template :edit
+      end
+    end
+
+    it_behaves_like "requires sign in" do
+      fraud = Fabricate(:fraud)
+      let(:action) { patch :update, id: fraud.id, fraud: Fabricate.attributes_for(:fraud) }
+    end
+  end
 end
